ea93fbabca63ebd80b0f5f2aa247159b1249f255
ShogoTsunoda <tsunoda.s.09@gmail.com>
2025-09-19 18:06:26 +0900

    Prettierの導入

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// prettier.config.js
+module.exports = {
+  semi: true, // 行末にセミコロンを付ける
+  singleQuote: true, // シングルクォートに統一
+  trailingComma: 'all', // 末尾カンマあり（複数行）
+  tabWidth: 2, // インデント幅 2
+  printWidth: 100, // 1行の最大長
+  bracketSpacing: true, // { foo: bar } のスペース有り
+  arrowParens: 'always', // (x) => ... の括弧を省略しない
+};
